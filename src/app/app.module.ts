c590224c317dc925c0ab0989c544462a61c4a226
Mohammed_Elaouri <mohammedelaouri@gmail.com>
2024-09-12 20:12:36 +0100

    add initial files

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsConditionsComponent } from './pages/terms-conditions/terms-conditions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './components/subscription-form/subscription-form.component';
+import { CommentsFormComponent } from './components/commments/comments-form/comments-form.component';
+import { CommentsListComponent } from './components/commments/comments-list/comments-list.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsConditionsComponent,
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    CommentsFormComponent,
+    CommentsListComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
